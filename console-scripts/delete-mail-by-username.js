a3c920bf87997885c4a2008cdbd86be09121885e
mandy-h <16712197+mandy-h@users.noreply.github.com>
2026-03-21 16:04:51 -0400

    Add logic to ignore saved messages and batch network requests

--- console-scripts/delete-mail-by-username.js
+++ console-scripts/delete-mail-by-username.js
@@ -4,22 +4,49 @@
  * Once mail is deleted, it's gone forever, so make sure you want to delete all these messages before you press the Enter key.
  * Once the mail is deleted, a message will be printed to the console. Refresh the page to see the updated inbox.
  */
-var usernames = [
-  'Cherry',
-  'ClickCritters',
-  'goclickcritters',
-  'Ringmaster Riley',
-];
-var selector = usernames
-  .map((username) => `#mail_heading [href*="/user/${username}"]`)
-  .join(',');
-var profileLinks = document.querySelectorAll(selector);
 
-var requests = Array.from(profileLinks).map((link) => {
-  const mailToDelete = link.parentElement.nextElementSibling.querySelector('a').href;
-  return fetch(mailToDelete);
-});
+(async function () {
+  const usernames = [
+    'Cherry',
+    'ClickCritters',
+    'goclickcritters',
+    'Ringmaster Riley',
+  ];
 
-Promise.all(requests).then(() => {
-  console.log('Deleted ' + profileLinks.length + ' messages');
-});
+  const selector = usernames
+    .map((username) => `#mail_heading [href*="/user/${username}"]`)
+    .join(',');
+  const links = document.querySelectorAll(selector);
+  let savedMailCount = 0;
+
+  const mailToDelete = Array.from(links).reduce((acc, link) => {
+    const heading = link.closest('#mail_heading');
+    const isMailSaved = heading.textContent.toLowerCase().includes('(saved)');
+    if (isMailSaved) {
+      savedMailCount++;
+    } else {
+      acc.push(heading.querySelector('[href*="mail.php?deleteid="]').href);
+    }
+    return acc;
+  }, []);
+
+  console.log('Found', links.length, 'messages');
+  await batchProcess(mailToDelete);
+  console.log('Deleted', (links.length - savedMailCount), 'messages');
+  console.log('Ignored', savedMailCount, 'saved messages');
+})();
+
+async function batchProcess(tasks, batchSize = 5) {
+  for (let i = 0; i < tasks.length; i += batchSize) {
+    const batch = tasks.slice(i, i + batchSize);
+    await Promise.all(batch.map(handleLink));
+  }
+}
+
+async function handleLink(url) {
+  try {
+    await fetch(url);
+  } catch (err) {
+    console.error('Failed:', url, err);
+  }
+}
